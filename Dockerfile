56aa38ff326ba77b3cc8391418a5310f933884ec
Leonard Mocanu <leonard.mocanu@ymail.com>
2018-03-23 11:20:33 +0200

    Added busybox build script

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM ubuntu:16.04
 
 ENV KERNEL_VERSION=4.12.3
+ENV BUSYBOX_VERSION=1.27.1
 
 RUN apt-get update && \
 	DEBIAN_FRONTEND=noninteractive apt-get install -yq wget bc build-essential gawk cpio xorriso && \
---
+++ build_busybox.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -ex
+
+if [ ! -f busybox.tar.bz2 ]; then
+	wget -q -O busybox.tar.bz2 http://busybox.net/downloads/busybox-$BUSYBOX_VERSION.tar.bz2
+fi
+
+if [ -d busybox-$BUSYBOX_VERSION ]; then
+	rm -rf busybox-$BUSYBOX_VERSION
+fi
+
+tar -xjf busybox.tar.bz2
+
+cd busybox-$BUSYBOX_VERSION
+
+make -j5 distclean defconfig
+sed -i "s/.*CONFIG_STATIC.*/CONFIG_STATIC=y/" .config
+
+make -j5 busybox install
+cd _install
+rm -f linuxrc
+mkdir dev proc sys
+echo '#!/bin/sh' > init
+echo 'dmesg -n 1' >> init
+echo 'mount -t devtmpfs none /dev' >> init
+echo 'mount -t proc none /proc' >> init
+echo 'mount -t sysfs none /sys' >> init
+echo 'setsid cttyhack /bin/sh' >> init
+chmod +x init
+
+find . | cpio -R root:root -H newc -o | gzip > ../isoimage/rootfs.gz
+
+cd ..
+set +ex
