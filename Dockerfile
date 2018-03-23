c91247222a7911b509b25d0c7b2b66d47611592a
Leonard Mocanu <leonard.mocanu@ymail.com>
2018-03-23 13:00:37 +0200

    Added syslinux and image build

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM ubuntu:16.04
 
 ENV KERNEL_VERSION=4.12.3
 ENV BUSYBOX_VERSION=1.27.1
+ENV SYSLINUX_VERSION=6.03
 
 RUN apt-get update && \
 	DEBIAN_FRONTEND=noninteractive apt-get install -yq wget bc build-essential gawk cpio xorriso && \
---
+++ build_syslinux.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+set -ex
+
+if [ ! -f syslinux.tar.xz ]; then
+	wget -q -O syslinux.tar.xz http://kernel.org/pub/linux/utils/boot/syslinux/syslinux-$SYSLINUX_VERSION.tar.xz
+fi
+
+if [ -d syslinux-$SYSLINUX_VERSION ]; then
+	rm -rf syslinux-$SYSLINUX_VERSION
+fi
+
+tar -xf syslinux.tar.xz
+
+cd isoimage
+
+cp ../syslinux-$SYSLINUX_VERSION/bios/core/isolinux.bin .
+cp ../syslinux-$SYSLINUX_VERSION/bios/com32/elflink/ldlinux/ldlinux.c32 .
+echo 'default kernel.xz initrd=rootfs.gz' > ./isolinux.cfg
+
+xorriso \
+  -as mkisofs \
+  -o ../minimal_linux_live.iso \
+  -b isolinux.bin \
+  -c boot.cat \
+  -no-emul-boot \
+  -boot-load-size 4 \
+  -boot-info-table \
+  ./
+
+cd ..
+set +ex
