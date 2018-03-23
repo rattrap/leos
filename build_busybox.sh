93e4c31d1d40b84ad36bd804dff24ef83324fc0b
Leonard Mocanu <leonard.mocanu@ymail.com>
2018-03-23 14:37:03 +0200

    Added rootfs

--- build_busybox.sh
+++ build_busybox.sh
@@ -21,14 +21,8 @@ sed -i "s/.*CONFIG_STATIC.*/CONFIG_STATIC=y/" .config
 make -j5 busybox install
 cd _install
 rm -f linuxrc
-mkdir dev proc sys
-echo '#!/bin/sh' > init
-echo 'dmesg -n 1' >> init
-echo 'mount -t devtmpfs none /dev' >> init
-echo 'mount -t proc none /proc' >> init
-echo 'mount -t sysfs none /sys' >> init
-echo 'setsid cttyhack /bin/sh' >> init
-chmod +x init
+
+cp -Rv ../rootfs/* .
 
 find . | cpio -R root:root -H newc -o | gzip > ../../isoimage/rootfs.gz
 
